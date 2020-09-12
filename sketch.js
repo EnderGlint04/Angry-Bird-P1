d4fd5c07c2d10b12ae8887366bfae6a9cb95bef1
EnderGlint04 <69745312+EnderGlint04@users.noreply.github.com>
2020-09-11 23:17:55 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,52 +3,58 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1;
-
+var box1, pig1;
+var bg;
+function preload(){
+    bg = loadImage("sprites/bg.png");
+}
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-    ground = new Ground(600,height,1200,20);
-    bird = new Bird(200,200);
     
-    //layer 1
-    box1 = new Box(700,300,70,70);
-    box2 = new Box(900,300,70,70);
-    pig1 = new Pig(800,300);
-    log = new Log(800,280,300,PI/2)
+    ground = new Ground(600,height,1200,20)
 
-    //layer 2
-    box3 = new Box(700,205,70,70);
-    box4 = new Box(900,205,70,70);
-    pig2 = new Pig(800,255);
-    log1 = new Log(800,190,300,PI/2);
-    //Topping
-    Abox = new Box(800,115,70,70);
-    Tlog = new Log(850,115,135,-PI/7);
-    Tlog2 = new Log(750,115,125,PI/7);
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(100,100);
 
 }
 
 function draw(){
-    background(15,40,240);
+    background(bg);
     Engine.update(engine);
-    ground.display();
-    bird.display();
-
-    //Layer 1
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
     box1.display();
     box2.display();
+    ground.display();
     pig1.display();
-    log.display();
-    //Layer 2
+    log1.display();
+
     box3.display();
     box4.display();
-    pig2.display();
-    log1.display();
-    //Topping
-    Abox.display();
-    Tlog.display();
-    Tlog2.display();
-;}
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+}
